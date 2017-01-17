fa9c587347392a0e02e5e603f2a273d94fcb8aee
Renan Cavichi <renancavichi@gmail.com>
2017-01-17 20:41:54 -0200

    Cria sistema de Migration e a tabela com o primeiro registro.

---
+++ Migrations/001-17-01-2017-CriaMigrationTable.sql
@@ -0,0 +1,26 @@
+/*
+ Migration  : 001
+ Developer  : Renan Cavichi
+ Data       : 17-01-2017
+ Description: Create Migration Table
+*/
+
+-- ----------------------------
+--  Table structure for migration
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."migration";
+CREATE TABLE "public"."migration" (
+	"id" int4 NOT NULL,
+	"description" text NOT NULL COLLATE "default",
+	"created_at" date NOT NULL,
+	"inserted_at" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+ALTER TABLE "public"."migration" OWNER TO "pg_aventurebox";
+
+-- ----------------------------
+--  Primary key structure for table migration
+-- ----------------------------
+ALTER TABLE "public"."migration" ADD PRIMARY KEY ("id") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+INSERT INTO migration VALUES(1,'Cria Tabela de Migrations','2017-01-17', now());
