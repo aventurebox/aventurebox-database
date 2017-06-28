1f7be88b28824f21a6ff07448d45ab2343e41c57
Paulo de Moura Neto <paulo.moura.neto@gmail.com>
2017-06-27 21:32:48 -0300

    Altera user_profile nova coluna email_failed

--- Migrations/003-21-06-2017-AlterTablesUser.sql
+++ Migrations/003-21-06-2017-AlterTablesUser.sql
@@ -9,4 +9,6 @@ ALTER TABLE user_temp ALTER COLUMN user_ TYPE character varying(40);
 ALTER TABLE user_login ALTER COLUMN user_ TYPE character varying(40);
 ALTER TABLE user_login_log ALTER COLUMN user_ TYPE character varying(40);
 
-INSERT INTO migration VALUES(3,'Altera tamanho da coluna user_ das tabelas user_temp, user_login, user_login_log','2017-06-21', now());
+alter table user_profile add email_failed boolean default false;
+
+INSERT INTO migration VALUES(3,'Altera tamanho da coluna user_ das tabelas user_temp, user_login, user_login_log. Altera user_profile nova coluna email_failed','2017-06-21', now());
