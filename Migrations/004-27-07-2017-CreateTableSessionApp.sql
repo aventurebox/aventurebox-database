73413d5f3ba1c62bd58bc8683b9e81589ffbc16e
Paulo de Moura Neto <paulo.moura.neto@gmail.com>
2017-07-27 20:33:26 -0300

    Migration 004 - Adiciona permissão ao usuário pg_aventurebox

--- Migrations/004-27-07-2017-CreateTableSessionApp.sql
+++ Migrations/004-27-07-2017-CreateTableSessionApp.sql
@@ -15,4 +15,10 @@ data text
 );
 ALTER TABLE session_app ADD FOREIGN KEY(id_user) REFERENCES user_login(id);
 
+grant select on all tables in schema public to pg_aventurebox;
+grant insert on all tables in schema public to pg_aventurebox;
+grant update on all tables in schema public to pg_aventurebox;
+grant delete on all tables in schema public to pg_aventurebox;
+grant all privileges on all sequences in schema public to pg_aventurebox;
+
 INSERT INTO migration VALUES(4,'Cria tabela de sessão de usuários','2017-07-27', now());
