b7d997f9ba41fdbac938f342fb2c9daef0c0851d
Renan Cavichi <renancavichi@gmail.com>
2019-10-08 19:53:36 -0300

    Add lang notification session

---
+++ Migrations/016-06-10-2019-AlterTableSessionAppLangNotification.sql
@@ -0,0 +1,15 @@
+/*
+ Migration  : 016 Developer  : Renan Cavichi
+ Data       : 06-10-2019
+ Description: Alter Table Session App Lang Notification
+*/
+
+ALTER TABLE session_app ADD COLUMN lang VARCHAR (5) NOT NULL DEFAULT 'en-us';
+
+grant select on all tables in schema public to pg_aventurebox;
+grant insert on all tables in schema public to pg_aventurebox;
+grant update on all tables in schema public to pg_aventurebox;
+grant delete on all tables in schema public to pg_aventurebox;
+grant all privileges on all sequences in schema public to pg_aventurebox;
+
+INSERT INTO migration VALUES(16,'Altera tabela session_app para incluir idioma de notificação','2019-10-06', now());
