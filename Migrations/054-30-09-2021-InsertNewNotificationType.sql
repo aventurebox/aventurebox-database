f28575f434d0ba935d718504518e6b382f2da874
Giselle Hoekveld Silva <gisellehoekveld@gmail.com>
2021-10-27 09:19:34 -0300

    Migration 54 - Insere mention na tabela notification_type

--- Migrations/054-30-09-2021-InsertNewNotificationType.sql
+++ Migrations/054-30-09-2021-InsertNewNotificationType.sql
@@ -5,12 +5,7 @@
 */
 
 INSERT INTO notification_type(id, name) VALUES (17, 'reply'), (18, 'rox_comment'), (19, 'rox_reply');
-
-grant select on all tables in schema public to "user-production";
-grant insert on all tables in schema public to "user-production";
-grant update on all tables in schema public to "user-production";
-grant delete on all tables in schema public to "user-production";
-grant all privileges on all sequences in schema public to "user-production";
+INSERT INTO notification_type(id, name) VALUES (20, 'mention');
 
 INSERT INTO migration VALUES(54, 'Insere novos valores na tabela notification_type', '2021-10-01', now());
 
