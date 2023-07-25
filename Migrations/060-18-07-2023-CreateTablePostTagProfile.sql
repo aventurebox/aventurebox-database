1464a62f642d7291bbdc661619b20ea154046dc4
Giselle Hoekveld <gisellehoekveld@gmail.com>
2023-07-25 10:32:18 -0300

    Migration 60 - Altera nome da tabela post_tag

--- Migrations/060-18-07-2023-CreateTablePostTagProfile.sql
+++ Migrations/060-18-07-2023-CreateTablePostTagProfile.sql
@@ -1,10 +1,10 @@
 /*
  Migration  : 060 Developer  : Giselle Hoekveld
  Data       : 18-07-2023
- Description: Cria tabela post_tag_profile e insere novos tipos de notificações
+ Description: Cria tabela post_tag_profile_profile e insere novos tipos de notificações
 */
 
-CREATE TABLE post_tag_profile(
+CREATE TABLE post_tag_profile_profile(
 	id SERIAL NOT NULL PRIMARY KEY,
 	id_post INT NOT NULL,
 	id_user INT NOT NULL,
@@ -15,10 +15,10 @@ CREATE TABLE post_tag_profile(
 	FOREIGN KEY (id_user) REFERENCES user_profile(id) ON DELETE CASCADE
 );
 
-INSERT INTO notification_type(id, name) VALUES (21, 'post_tag_profile');
+INSERT INTO notification_type(id, name) VALUES (21, 'post_tag_profile_profile');
 INSERT INTO notification_type(id, name) VALUES (22, 'comment_on_post_profile_tagged');
-INSERT INTO notification_type(id, name) VALUES (23, 'post_tag_approved');
-INSERT INTO notification_type(id, name) VALUES (24, 'post_tag_denied');
+INSERT INTO notification_type(id, name) VALUES (23, 'post_tag_profile_approved');
+INSERT INTO notification_type(id, name) VALUES (24, 'post_tag_profile_denied');
 
 grant select on all tables in schema public to "user-production";
 grant insert on all tables in schema public to "user-production";
@@ -26,4 +26,4 @@ grant update on all tables in schema public to "user-production";
 grant delete on all tables in schema public to "user-production";
 grant all privileges on all sequences in schema public to "user-production";
 
-INSERT INTO migration VALUES(60, 'Cria tabela post_tag_profile e insere novos tipos de notificações', '2023-7-18', now());
+INSERT INTO migration VALUES(60, 'Cria tabela post_tag_profile_profile e insere novos tipos de notificações', '2023-7-18', now());
