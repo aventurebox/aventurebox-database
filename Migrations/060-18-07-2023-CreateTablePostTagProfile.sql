93978ecf14d9db081f9145925402fd8a8a5a016e
Giselle Hoekveld <gisellehoekveld@gmail.com>
2023-07-29 17:43:30 -0300

    Migration 60 - Adiciona tabela de log para post_tag_profile

--- Migrations/060-18-07-2023-CreateTablePostTagProfile.sql
+++ Migrations/060-18-07-2023-CreateTablePostTagProfile.sql
@@ -4,6 +4,26 @@
  Description: Cria tabela post_tag_profile e insere novos tipos de notificações
 */
 
+--------------- AVENTUREBOX LOG --------------------
+CREATE TABLE post_tag_profile_log(
+	id SERIAL NOT NULL PRIMARY KEY,
+	id_post INT NOT NULL,
+	id_user INT NOT NULL,
+	notified BOOLEAN DEFAULT FALSE,
+	show_profile BOOLEAN DEFAULT TRUE,
+	approved BOOLEAN NOT NULL DEFAULT FALSE,
+	type_log INTEGER,
+	dt_log TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+grant select on all tables in schema public to "user-production";
+grant insert on all tables in schema public to "user-production";
+grant update on all tables in schema public to "user-production";
+grant delete on all tables in schema public to "user-production";
+grant all privileges on all sequences in schema public to "user-production";
+--------------- END AVENTUREBOX LOG --------------------
+
+--------------- AVENTUREBOX --------------------
 CREATE TABLE post_tag_profile(
 	id SERIAL NOT NULL PRIMARY KEY,
 	id_post INT NOT NULL,
@@ -20,6 +40,46 @@ INSERT INTO notification_type(id, name) VALUES (22, 'comment_on_post_profile_tag
 INSERT INTO notification_type(id, name) VALUES (23, 'post_tag_profile_approved');
 INSERT INTO notification_type(id, name) VALUES (24, 'post_tag_profile_denied');
 
+--------------- FOREIGN TABLES 
+CREATE FOREIGN TABLE post_tag_profile_log (
+	id_post INT NOT NULL,
+	id_user INT NOT NULL,
+	notified BOOLEAN DEFAULT FALSE,
+	show_profile BOOLEAN DEFAULT TRUE,
+	approved BOOLEAN NOT NULL DEFAULT FALSE,
+	type_log INTEGER,
+	dt_log TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+) SERVER aventurebox_log OPTIONS (table_name 'post_tag_profile_log');
+
+--------------- TRIGGERS
+CREATE FUNCTION fn_post_tag_profile_delete()
+	RETURNS trigger
+	LANGUAGE 'plpgsql'
+	COST 100
+	VOLATILE NOT LEAKPROOF 
+AS $BODY$
+BEGIN
+INSERT INTO post_tag_profile_log(type_log,dt_log,id_post,id_user,notified,show_profile,approved,date)
+	VALUES (3,NOW(),old.id_post,old.id_user,old.notified,old.show_profile,old.approved,old.date);
+	return old;
+	END; 
+	$BODY$;
+CREATE TRIGGER tg_post_tag_profile_delete AFTER DELETE ON post_tag_profile FOR EACH ROW EXECUTE PROCEDURE fn_post_tag_profile_delete();
+
+CREATE FUNCTION fn_post_tag_profile_update()
+	RETURNS trigger
+	LANGUAGE 'plpgsql'
+	COST 100
+	VOLATILE NOT LEAKPROOF 
+AS $BODY$
+BEGIN
+INSERT INTO post_tag_profile_log(type_log,dt_log,id_post,id_user,notified,show_profile,approved,date)
+	VALUES (2,NOW(),old.id_post,old.id_user,old.notified,old.show_profile,old.approved,old.date);
+	return old;
+	END; 
+	$BODY$;
+CREATE TRIGGER tg_post_tag_profile_update AFTER DELETE ON post_tag_profile FOR EACH ROW EXECUTE PROCEDURE fn_post_tag_profile_update();
+
 grant select on all tables in schema public to "user-production";
 grant insert on all tables in schema public to "user-production";
 grant update on all tables in schema public to "user-production";
@@ -27,3 +87,4 @@ grant delete on all tables in schema public to "user-production";
 grant all privileges on all sequences in schema public to "user-production";
 
 INSERT INTO migration VALUES(60, 'Cria tabela post_tag_profile e insere novos tipos de notificações', '2023-7-18', now());
+--------------- END AVENTUREBOX --------------------
