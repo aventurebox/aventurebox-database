6b5fe03240b93d483faa34e3eb38702ae25fee4a
Giselle Hoekveld <gisellehoekveld@gmail.com>
2023-08-01 15:53:14 -0300

    Migration 61 - Create materialized views with verified column

--- Migrations/061-28-07-2023-AlterTableUserProfile.sql
+++ Migrations/061-28-07-2023-AlterTableUserProfile.sql
@@ -43,6 +43,108 @@ INNER JOIN user_suggestion us ON up.id = us.id_user
 ORDER BY us.first DESC, random();
 CREATE UNIQUE INDEX ON user_suggestion_view(id);
 
+DROP MATERIALIZED VIEW IF EXISTS post_suggestion_view;
+
+CREATE MATERIALIZED VIEW post_suggestion_view AS
+WITH 
+top_rox AS (
+	SELECT 
+	p.id, p.id_user, p.title, p.title_slug, p.description, p.token, 
+	p.content, p.dt_publication, p.panoramic_photo,
+	up.id AS user_id, up.user_, up.name, up.picture picture_u, up.token AS token_user, up.verified AS verified_u
+	FROM post p
+	INNER JOIN user_profile up ON p.id_user = up.id
+	WHERE p.published = TRUE
+	AND up.inactive = FALSE
+	AND up.suspended = FALSE
+	AND p.do_not_list = FALSE
+	ORDER BY (
+		SELECT COUNT(rox.id_user) FROM post_rox AS rox 
+		JOIN user_profile ups ON ups.id = rox.id_user 
+		WHERE rox.id_post = p.id AND ups.inactive = FALSE AND ups.suspended = FALSE
+	) DESC
+),
+suggestions AS (
+	SELECT * FROM top_rox
+	WHERE id IN (SELECT id_post FROM post_suggestion)
+),
+rox_month AS (
+	SELECT * FROM top_rox
+	WHERE (dt_publication + interval '3 month' > NOW())
+	AND id NOT IN (SELECT id FROM suggestions)
+	LIMIT 500 * 0.15
+),
+rox_year AS (
+	SELECT * FROM top_rox
+	WHERE (dt_publication + interval '1 year' > NOW())
+	AND id NOT IN (SELECT * FROM (SELECT id FROM suggestions UNION ALL SELECT id FROM rox_month) t1)
+	LIMIT 500 * 0.25
+),
+rox_platform AS (
+	SELECT * FROM top_rox
+	WHERE id NOT IN (SELECT * FROM (SELECT id FROM suggestions UNION ALL SELECT id FROM rox_month UNION ALL SELECT id FROM rox_year) t2)
+	LIMIT 500 * 0.6
+)
+SELECT * FROM (
+	SELECT * FROM rox_month UNION ALL
+	SELECT * FROM rox_year UNION ALL
+	SELECT * FROM rox_platform UNION ALL
+	SELECT * FROM suggestions
+) posts;
+CREATE UNIQUE INDEX ON post_suggestion_view (id);
+
+DROP MATERIALIZED VIEW IF EXISTS adventure_suggestion_view;
+
+CREATE MATERIALIZED VIEW adventure_suggestion_view AS
+WITH 
+top_rox AS (
+	SELECT 
+	a.id, a.id_user, a.title, a.title_slug, a.description, a.token, 
+	a.dt_start, a.dt_end, a.dt_publication, a.id_item_report, a.id_item_photo,
+	a.id_item_video, a.id_item_tracklog, a.id_item_checklist, a.panoramic_photo, a.picture, 
+	a.location[0] lat, a.location[1] lng, 
+	up.id AS user_id, up.user_, up.name, up.picture picture_u, up.token AS token_user, up.verified AS verified_u
+	FROM adventure a
+	INNER JOIN user_profile up ON a.id_user = up.id
+	WHERE a.published = TRUE
+	AND up.inactive = FALSE
+	AND up.suspended = FALSE
+	AND a.do_not_list = FALSE
+	ORDER BY (
+		SELECT COUNT(rox.id_user) FROM adventure_rox AS rox 
+		JOIN user_profile ups ON ups.id = rox.id_user 
+		WHERE rox.id_adventure = a.id AND ups.inactive = FALSE
+	) DESC
+),
+suggestions AS (
+	SELECT * FROM top_rox
+	WHERE id IN (SELECT id_adventure FROM adventure_suggestion)
+),
+rox_month AS (
+	SELECT * FROM top_rox
+	WHERE (dt_publication + interval '3 month' > NOW())
+	AND id NOT IN (SELECT id FROM suggestions)
+	LIMIT 500 * 0.15
+),
+rox_year AS (
+	SELECT * FROM top_rox
+	WHERE (dt_publication + interval '1 year' > NOW())
+	AND id NOT IN (SELECT * FROM (SELECT id FROM suggestions UNION ALL SELECT id FROM rox_month) t1)
+	LIMIT 500 * 0.25
+),
+rox_platform AS (
+	SELECT * FROM top_rox
+	WHERE id NOT IN (SELECT * FROM (SELECT id FROM suggestions UNION ALL SELECT id FROM rox_month UNION ALL SELECT id FROM rox_year) t2)
+	LIMIT 500 * 0.6
+)
+SELECT * FROM (
+	SELECT * FROM rox_month UNION ALL
+	SELECT * FROM rox_year UNION ALL
+	SELECT * FROM rox_platform UNION ALL
+	SELECT * FROM suggestions
+) adventures;
+CREATE UNIQUE INDEX ON adventure_suggestion_view (id);
+
 grant select on all tables in schema public to "user-production";
 grant insert on all tables in schema public to "user-production";
 grant update on all tables in schema public to "user-production";
